16d7cb1d5512bd91a2b1a8c35b720be13451b557
bugzhang <nuptunee@gmail.com>
2019-10-29 00:42:24 +0800

    fix:fix test explain

--- test/chapter1/section1/1.1.13.test.js
+++ test/chapter1/section1/1.1.13.test.js
@@ -2,7 +2,7 @@ var transposit = require('../../../chapter1/section1/src/1.1.13');
 var expect = require('chai').expect;
 
 describe('1.1.13的测试', function () {
-	it('1.1.13的测试', function () {
+	it('1.1.13的测试结果', function () {
 		expect(JSON.stringify(transposit([[1, 2, 3], [1, 2, 3], [1, 2, 3]]))).to.be.equal(JSON.stringify([[1, 1, 1], [2, 2, 2], [3, 3, 3]]));
 	});
 });
--- test/chapter1/section1/1.1.14.test.js
+++ test/chapter1/section1/1.1.14.test.js
@@ -2,7 +2,7 @@ var lg = require('../../../chapter1/section1/src/1.1.14');
 var expect = require('chai').expect;
 
 describe('1.1.14的测试', function () {
-	it('1.1.14的测试', function () {
+	it('1.1.14的测试结果', function () {
 		expect(lg(5)).to.be.equal(2);
 		expect(lg(16)).to.be.equal(4);
 	});
