30eabc51e1e8bdc3624865a14e9d0ce3a68af10e
张崇 <nuptunee@gmail.com>
2019-10-31 11:58:20 +0800

    feat:ArrayQueue

--- src/chapter1/section3/ArrayQueue.js
+++ src/chapter1/section3/ArrayQueue.js
@@ -3,5 +3,34 @@
  */
 
 class Queue  {
+	constructor() {
+		this.dataStore = [];
+    this.front = front;
+    this.back = back;
+    this.length = length;
+	}
 
+	enqueue(item) {
+		this.dataStore.push(item);
+	}
+
+	dequeue() {
+		this.dataStore.shift();
+	}
+
+	toString() {
+		var ret = '';
+		this.dataStore.forEach((item) => {
+			ret +=  item;
+		});
+		return ret;
+	}
+
+	length() {
+		return this.length;
+	}
+
+	clear() {
+		this.length = 0;
+	}
 }
