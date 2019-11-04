9cd7dbf258f77bbbe5cf97b0607ec105a179245d
张崇 <nuptunee@gmail.com>
2019-11-04 14:27:27 +0800

    feat:NodeQueue

--- src/chapter1/section3/NodeQueue.js
+++ src/chapter1/section3/NodeQueue.js
@@ -11,7 +11,47 @@ class Node {
 
 class Queue {
 	constructor() {
-		this.comment
+		this.front = null; // head of Queue
+		this.rear = null; // tail of Queue
+		this.length = 0; // length of Queue
+	}
+
+	push(elemenet) {
+		let current = null;
+		let node = new Node(elemenet);
+
+		if (this.length === 0) {
+			this.front = node;
+			this.rear = node;
+			this.length ++;
+			return true;
+		} else {
+			current = this.rear;
+			current.next = node;
+			this.rear = node;
+			this.length ++;
+			return true;
+		}
+	}
+
+	pop() {
+		let current = null;
+		
+		if (this.length !== 0) {
+			current = this.front;
+			this.front = current.next;
+			this.length --;
+			return true;
+		} else {
+			return null;
+		}
+	}
+
+	clear() {
+		this.front = null;
+		this.rear = null;
+		this.length = 0;
+		return true;
 	}
 }
 
