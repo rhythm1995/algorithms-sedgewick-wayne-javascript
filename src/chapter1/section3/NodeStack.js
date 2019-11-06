38f2028555fd17ca69bc471c3fd9c4db6b00637a
张崇 <nuptunee@gmail.com>
2019-11-06 20:24:12 +0800

    feat: start nodestack

---
+++ src/chapter1/section3/NodeStack.js
@@ -0,0 +1,29 @@
+/**
+ * 链表实现栈
+ */
+
+class Node {
+	constructor(element) {
+		this.element = element;
+    this.next = null;
+	}
+}
+
+class Stack {
+	constructor() {
+		this.next = null;
+		this.length = 0; // length of Stack
+	}
+
+	push() {
+		
+	}
+
+	pop() {
+
+	}
+
+	
+}
+
+module.exports = Stack;
